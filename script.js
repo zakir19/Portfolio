b66c5e9c05fd2f7678dc242f01a87b3218a606aa
Zakir Husain <121443426+zakir19@users.noreply.github.com>
2024-08-28 20:35:41 +0530

    script.js

--- script.js
+++ script.js
@@ -17,32 +17,32 @@ function revealToSpan() {
 }
 revealToSpan()
 
-var t1 = gsap.timeline();
+// var t1 = gsap.timeline();
 
-t1
-    .from(".child .parent", {
-        x: 100,
-        delay: 1,
-        stagger: .2,
-        duration: 1,
-        ease: Power3.easeInOut
-    })
-    .to(".parent .child", {
-        y: "-100%",
-        duration: 1,
-        ease: Circ.easeInOut
-    })
-    .to("#loader", {
-        height: 0,
-        duration: 1,
-        ease: Circ.easeInOut
-    })
-    .to("#grey", {
-        height: "100%",
-        duration: 1,
-        delay: -1,
-        ease: Circ.easeinOut
-    })
+// t1
+//     .from(".child .parent", {
+//         x: 100,
+//         delay: 1,
+//         stagger: .2,
+//         duration: 1,
+//         ease: Power3.easeInOut
+//     })
+//     .to(".parent .child", {
+//         y: "-100%",
+//         duration: 1,
+//         ease: Circ.easeInOut
+//     })
+//     .to("#loader", {
+//         height: 0,
+//         duration: 1,
+//         ease: Circ.easeInOut
+//     })
+//     .to("#grey", {
+//         height: "100%",
+//         duration: 1,
+//         delay: -1,
+//         ease: Circ.easeinOut
+//     })
 
 
 function toggleMenu() {
